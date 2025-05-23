234c189093f04e3794f2eda72118191745f4d280
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-05-23 22:56:03 +0300

    🎉 added solichain token

---
+++ script/CrossChainToken.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/cross-chain-erc20/SolichainToken.sol";
+
+contract DeploySolichainToken is Script {
+    function run() external {
+        // Load environment variables
+        string memory privateKey = vm.envString("PRIVATE_KEY");
+
+        // Convert private key string to uint256
+        uint256 pk = vm.parseUint(privateKey);
+
+        // Set the deployer's private key
+        vm.startBroadcast(pk);
+
+        // Define constructor parameters
+        // uint256 initialSupply = 1_000_000_000_000000000000000000;
+        // address router = 0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59; // router Ethereum sepolia, you can get it from Helper.sol
+        // address link = 0x779877A7B0D9E8603169DdbD7836e478b4624789; // https://docs.chain.link/ccip/supported-networks/v1_2_0/testnet#ethereum-sepolia-base-sepolia
+
+        uint256 initialSupply = 0;
+        address router = 0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93; // router Base sepolia, you can get it from Helper.sol
+        address link = 0xE4aB69C077896252FAFBD49EFD26B5D171A32410; // https://docs.chain.link/resources/link-token-contracts
+
+        // Deploy the SolichainToken contract
+        SolichainToken token = new SolichainToken(initialSupply, router, link);
+
+        vm.stopBroadcast();
+
+        // Output the deployed contract address
+        console.log("SolichainToken deployed at:", address(token));
+        console.log(
+            "My balance is:",
+            token.balanceOf(0x85dA99c8a7C2C95964c8EfD687E95E632Fc533D6)
+        );
+    }
+}
+
+// deploying on ethereum sepolia
+// forge script script/SolichainToken.sol --rpc-url $ETHEREUM_SEPOLIA_RPC_URL --private-key $PRIVATE_KEY --broadcast
+// ##### sepolia
+// ✅  [Success]Hash: 0x06ed99a8beb6d38ff862294c64de092079d8bd8ec130ba9e0d9ac924a5a0e919
+// Contract Address: 0xBD1e6CB60c174d69cAAC4Be32CeC3CbCBFd80F16
+// Block: 6733498
+// Paid: 0.037717886021221278 ETH (1688553 gas * 22.337401326 gwei)
+
+// flatten the contract
+// forge flatten src/cross-chain-erc20/SolichainToken.sol --output flattened/SolichainToken.sol
+
+// since we do need to verify the contract, we do need to cast the parameters that we passed when we deployed.
+// parameters: 1000000000000000000000000000 "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59" "0x779877A7B0D9E8603169DdbD7836e478b4624789"
+// the command: cast abi-encode "constructor(uint256,address,address)" 1000000000000000000000000000 "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59" "0x779877A7B0D9E8603169DdbD7836e478b4624789"
+// output: 0x0000000000000000000000000000000000000000033b2e3c9fd0803ce80000000000000000000000000000000bf3de8c5d3e8a2b34d2beeb17abfcebaf363a59000000000000000000000000779877a7b0d9e8603169ddbd7836e478b4624789
+
+// verify contract
+// forge verify-contract --chain-id 11155111 --num-of-optimizations 200 --compiler-version v0.8.19 --watch "0xBD1e6CB60c174d69cAAC4Be32CeC3CbCBFd80F16" "src/cross-chain-erc20/SolichainToken.sol:SolichainToken" --etherscan-api-key "PUT YOUR ETHER SCAN API VALUE HERE" --constructor-args 0x0000000000000000000000000000000000000000033b2e3c9fd0803ce80000000000000000000000000000000bf3de8c5d3e8a2b34d2beeb17abfcebaf363a59000000000000000000000000779877a7b0d9e8603169ddbd7836e478b4624789
+
+// deploying on ethereum sepolia
+// forge script script/SolichainToken.sol --rpc-url $BASE_SEPOLIA_RPC_URL --private-key $PRIVATE_KEY --broadcast
+
+//  base-sepolia
+// ✅  [Success]Hash: 0xa39568d1ee91455c20bdea262b6f4330b815607ddb6ddd1142d31db7a13d0f17
+// Contract Address: 0x6FdA56C57B0Acadb96Ed5624aC500C0429d59429
+// Block: 15581090
+// Paid: 0.00000164907035041 ETH (1648645 gas * 0.001000258 gwei)
+
+// flatten the contract note: since you are using the same contract you can skip the flatten because you made it before.
+// forge flatten src/cross-chain-erc20/SolichainToken.sol --output flattened/SolichainToken.sol
+
+// since we do need to verify the contract, we do need to cast the parameters that we passed when we deployed.
+// cast abi-encode "constructor(uint256,address,address)" 0  "0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93" "0xE4aB69C077896252FAFBD49EFD26B5D171A32410"
+// 0x0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000d3b06cebf099ce7da4accf578aaebfdbd6e88a93000000000000000000000000e4ab69c077896252fafbd49efd26b5d171a32410
+
+// just as note for base sepolia login my username is: mohamadhammoud98
+
+// 1000000000000000000000
---
+++ src/cross-chain-erc20/SolichainToken.sol
@@ -0,0 +1,207 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title Solichain Token
+ * @dev An ERC20 token that supports cross-chain transfers using Chainlink CCIP.
+ */
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+contract SolichainToken is ERC20, CCIPReceiver, Ownable {
+    /// @notice Mapping to track locked tokens on the source chain
+    mapping(address => uint256) public lockedBalances;
+
+    /// @notice Mapping to store the parallel token contract addresses on multiple chains
+    mapping(uint64 => address) public parallelTokenAddresses;
+
+    /// @notice Link token interface to handle LINK token interactions
+    LinkTokenInterface public linkToken;
+
+    /// @notice Emitted when a cross-chain transfer is initiated
+    /// @param sender The address initiating the transfer
+    /// @param destinationChain The chain ID of the destination chain
+    /// @param amount The amount of tokens being transferred
+    event CrossChainTransfer(
+        address indexed sender,
+        uint64 destinationChain,
+        uint256 amount
+    );
+
+    /// @notice Emitted when a parallel token address is set for a chain
+    /// @param chainId The chain ID for which the parallel token address is set
+    /// @param newParallelTokenAddress The address of the parallel token contract on the specified chain
+    event ParallelTokenAddressSet(
+        uint64 indexed chainId,
+        address indexed newParallelTokenAddress
+    );
+
+    /**
+     * @notice Constructor for the CrossChainToken
+     * @param initialSupply The initial supply of tokens to mint to the deployer
+     * @param router The address of the Chainlink CCIP Router contract
+     * @param link The address of the LINK token contract
+     */
+    constructor(
+        uint256 initialSupply,
+        address router,
+        address link
+    ) ERC20("Solichain Token", "SCT") CCIPReceiver(router) {
+        _mint(msg.sender, initialSupply);
+        linkToken = LinkTokenInterface(link);
+    }
+
+    /**
+     * @notice Sets the parallel token contract address for a specific chain
+     * @param chainId The chain ID of the target chain
+     * @param _parallelTokenAddress The address of the parallel token contract on the target chain
+     */
+    function setParallelTokenAddress(
+        uint64 chainId,
+        address _parallelTokenAddress
+    ) external onlyOwner {
+        parallelTokenAddresses[chainId] = _parallelTokenAddress;
+        emit ParallelTokenAddressSet(chainId, _parallelTokenAddress);
+    }
+
+    /**
+     * @notice Locks tokens and initiates a cross-chain transfer to the destination chain
+     * @param destinationChain The chain ID of the destination chain
+     * @param amount The amount of tokens to transfer
+     */
+    function lockTokens(uint64 destinationChain, uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
+        require(
+            parallelTokenAddresses[destinationChain] != address(0),
+            "Parallel token address not set for destination chain"
+        );
+
+        // Lock tokens
+        _transfer(msg.sender, address(this), amount);
+        lockedBalances[msg.sender] += amount;
+
+        // Send cross-chain message
+        _sendCrossChainMessage(destinationChain, amount);
+        emit CrossChainTransfer(msg.sender, destinationChain, amount);
+    }
+
+    /**
+     * @notice Burns tokens on the destination chain to unlock tokens on the source chain
+     * @param amount The amount of tokens to burn
+     * @param sourceChain The chain ID of the source chain where tokens will be unlocked
+     */
+    function burnTokens(uint256 amount, uint64 sourceChain) external {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
+        require(
+            parallelTokenAddresses[sourceChain] != address(0),
+            "Parallel token address not set for source chain"
+        );
+
+        // Burn tokens
+        _burn(msg.sender, amount);
+
+        // Send cross-chain message to unlock tokens on the source chain
+        _sendCrossChainMessage(sourceChain, amount);
+    }
+
+    /**
+     * @notice External getter function to retrieve the locked balance of an address
+     * @param account The address for which to retrieve the locked balance
+     * @return The amount of tokens locked for the given address
+     */
+    function getLockedBalance(address account) external view returns (uint256) {
+        return lockedBalances[account];
+    }
+
+    /**
+     * @notice External getter function to retrieve the parallel token address for a chain
+     * @param chainId The chain ID for which to retrieve the parallel token address
+     * @return The address of the parallel token contract on the specified chain
+     */
+    function getParallelTokenAddress(
+        uint64 chainId
+    ) external view returns (address) {
+        return parallelTokenAddresses[chainId];
+    }
+
+    /**
+     * @notice Internal function to send a cross-chain message via Chainlink CCIP
+     * @param destinationChain The chain ID of the destination chain
+     * @param amount The amount of tokens being transferred
+     */
+    function _sendCrossChainMessage(
+        uint64 destinationChain,
+        uint256 amount
+    ) internal {
+        // Prepare the cross-chain message
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(parallelTokenAddresses[destinationChain]),
+            data: abi.encode(msg.sender, amount),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: address(linkToken)
+        });
+
+        IRouterClient router = IRouterClient(getRouter());
+        uint256 fees = router.getFee(destinationChain, message);
+
+        // Approve the Router to spend LINK tokens for the fee
+        linkToken.approve(address(router), fees);
+
+        // Send the cross-chain message
+        router.ccipSend(destinationChain, message);
+    }
+
+    /**
+     * @notice Internal function called by the CCIP Router to handle incoming cross-chain messages
+     * @param any2EvmMessage The incoming cross-chain message
+     */
+    function _ccipReceive(
+        Client.Any2EVMMessage memory any2EvmMessage
+    ) internal override {
+        (address sender, uint256 amount) = abi.decode(
+            any2EvmMessage.data,
+            (address, uint256)
+        );
+
+        _mint(sender, amount);
+    }
+
+    /**
+     * @notice Withdraws LINK tokens (used for paying CCIP fees) to the contract owner
+     * @param amount The amount of LINK tokens to withdraw
+     */
+    function withdrawLink(uint256 amount) external onlyOwner {
+        require(
+            linkToken.balanceOf(address(this)) >= amount,
+            "Not enough LINK tokens"
+        );
+        linkToken.transfer(msg.sender, amount);
+    }
+
+    /**
+     * @notice Retrieves the required LINK fee for a cross-chain message
+     * @param destinationChain The chain ID of the destination chain
+     * @param amount The amount of tokens to transfer
+     * @return fees The amount of LINK tokens required as a fee
+     */
+    function getRequiredFee(
+        uint64 destinationChain,
+        uint256 amount
+    ) external view returns (uint256 fees) {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(parallelTokenAddresses[destinationChain]),
+            data: abi.encode(msg.sender, amount),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: address(linkToken)
+        });
+
+        IRouterClient router = IRouterClient(getRouter());
+        fees = router.getFee(destinationChain, message);
+    }
+}
